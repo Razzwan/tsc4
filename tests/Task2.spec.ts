2afc31a6006a424cf2016273660bea7fd2a8be3a
Razzwan <razvanlomov@gmail.com>
2023-08-09 16:01:56 +0300

    feat(2): first working version

--- tests/Task2.spec.ts
+++ tests/Task2.spec.ts
@@ -1,6 +1,6 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
-import { Task2 } from '../wrappers/Task2';
+import {Cell, toNano} from 'ton-core';
+import {getTuple1, getTuple2, parseData, Task2} from '../wrappers/Task2';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
 
@@ -35,4 +35,71 @@ describe('Task2', () => {
         // the check is done inside beforeEach
         // blockchain and task2 are ready to use
     });
+
+    // describe('multiply 2 tuples', () => {
+    //     it('just simple sum', async () => {
+    //         const res = await task2.getMultiply2Tuples([getTuple1([1, 1, 1]), getTuple1([2, 2, 2])]);
+    //         expect(res).toBe(6);
+    //     });
+    //
+    //     it('just 3x3 1', async () => {
+    //         const res = await task2.getMultiply2Tuples([getTuple1([1, 2, 3]), getTuple1([7, 9, 11])]);
+    //         expect(res).toBe(58);
+    //     });
+    //
+    //     it('just 3x3 2', async () => {
+    //         const res = await task2.getMultiply2Tuples([getTuple1([1, 2, 3]), getTuple1([8, 10, 12])]);
+    //         expect(res).toBe(64);
+    //     });
+    //
+    //     it('sum is 64', async () => {
+    //         const res = await task2.getMultiply2Tuples([
+    //           getTuple1([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
+    //           getTuple1([2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
+    //         ]);
+    //         expect(res).toBe(64);
+    //     });
+    // });
+
+    // describe('rotate 90 degrees', () => {
+    //     it ('rotate 90 degrees, 3x2', async () => {
+    //         const res = await task2.getTurnTuple90([getTuple2([[7, 8], [9, 10], [11, 12]])]);
+    //         expect(parseData(res)).toEqual([[7,9,11], [8,10,12]]);
+    //     });
+    // });
+
+    describe('multiply matrix', () => {
+        it('multiply simplest matrix', async () => {
+            const res = await task2.getMatrixResult([getTuple2([[1, 2, 3], [4, 5, 6]]), getTuple2([[7, 8], [9, 10], [11, 12]])]);
+            expect(parseData(res)).toEqual([[58, 64], [139, 154]]);
+            // expect(parseData(res)).toEqual(1);
+        });
+    });
 });
+
+
+/**
+ *
+ *
+ *
+ * A = [[1, 2, 3],
+ *      [4, 5, 6]]
+ *
+ * B = [[7, 8],
+ *      [9, 10],
+ *      [11, 12]]
+ *
+ * m = 2, n = 3, p = 2
+ *
+ * C = [[1*7 + 2*9 + 3*11, 1*8 + 2*10 + 3*12],
+ *      [4*7 + 5*9 + 6*11, 4*8 + 5*10 + 6*12]]
+ *
+ * C = [[58, 64],
+ *      [139, 154]]
+ *
+ *
+ *
+ *
+ *
+ *
+ */
--- wrappers/Task2.ts
+++ wrappers/Task2.ts
@@ -1,9 +1,61 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+    Address,
+    beginCell,
+    Cell,
+    Contract,
+    contractAddress,
+    ContractProvider,
+    Sender,
+    SendMode,
+    Tuple,
+    TupleItem,
+} from 'ton-core';
+import {TupleItemInt} from 'ton-core/src/tuple/tuple';
 
 export type Task2Config = {};
 
+export function parseData(tuple: Tuple | TupleItem): any {
+    if (!tuple.type && Array.isArray((tuple as any).items)) {
+        return (tuple as any).items.map((item: TupleItem) => parseData(item));
+    }
+    if (tuple.type === 'tuple') {
+        return tuple.items.map(item => parseData(item));
+    }
+    if (tuple.type === 'int') {
+        return Number(tuple.value);
+    }
+    return {};
+}
+
 export function task2ConfigToCell(config: Task2Config): Cell {
     return beginCell().endCell();
+}
+
+export type Matrix1 = Array<number>;
+
+export function getTuple1(arr: Matrix1): Tuple {
+    return {
+        type: 'tuple',
+        items: arr.map((n) => ({
+            type: 'int',
+            value: BigInt(n),
+        }))
+    };
+}
+
+export type Matrix2 = Array<Array<number>>;
+
+export function getTuple2(arr: Matrix2): Tuple {
+    return {
+        type: 'tuple',
+        items: arr.map((numbers) => ({
+            type: 'tuple',
+            items: numbers.map((n) => ({
+                type: 'int',
+                value: BigInt(n),
+            }))
+        }))
+    };
 }
 
 export class Task2 implements Contract {
@@ -25,5 +77,20 @@ export class Task2 implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async getTurnTuple90(provider: ContractProvider, options: [Tuple]): Promise<any> {
+        const result = await provider.get('turn_tuple_90', options);
+        return result.stack.readTuple();
+    }
+
+    async getMultiply2Tuples(provider: ContractProvider, options: [Tuple, Tuple]): Promise<number> {
+        const result = await provider.get('mult_2_tuples', options);
+        return result.stack.readNumber();
+    }
+
+    async getMatrixResult(provider: ContractProvider, options: [Tuple, Tuple]): Promise<any> {
+        const result = await provider.get('matrix_multiplier', options);
+        return result.stack.readTuple();
     }
 }
