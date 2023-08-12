bf776254f607489355e7c8253c2e9eea02d7220b
Razzwan <razvanlomov@gmail.com>
2023-08-12 18:44:47 +0300

    feat(3): first try

--- wrappers/Task3.ts
+++ wrappers/Task3.ts
@@ -1,50 +1,58 @@
 import {
-    Address,
-    beginCell,
-    Cell,
-    Contract,
-    contractAddress,
-    ContractProvider,
-    Sender,
-    SendMode, Slice, Tuple,
-    TupleItemCell, TupleItemInt, TupleReader
+	Address,
+	beginCell,
+	Cell,
+	Contract,
+	contractAddress,
+	ContractProvider,
+	Sender,
+	SendMode, Slice, Tuple,
+	TupleItemCell, TupleItemInt, TupleReader
 } from 'ton-core';
 import {TupleItemSlice} from 'ton-core/dist/tuple/tuple';
 
 export type Task3Config = {};
 
 export function task3ConfigToCell(config: Task3Config): Cell {
-    return beginCell().endCell();
+	return beginCell().endCell();
 }
 
 export class Task3 implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+	constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {
+	}
 
-    static createFromAddress(address: Address) {
-        return new Task3(address);
-    }
+	static createFromAddress(address: Address) {
+		return new Task3(address);
+	}
 
-    static createFromConfig(config: Task3Config, code: Cell, workchain = 0) {
-        const data = task3ConfigToCell(config);
-        const init = { code, data };
-        return new Task3(contractAddress(workchain, init), init);
-    }
+	static createFromConfig(config: Task3Config, code: Cell, workchain = 0) {
+		const data = task3ConfigToCell(config);
+		const init = {code, data};
+		return new Task3(contractAddress(workchain, init), init);
+	}
 
-    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
-        await provider.internal(via, {
-            value,
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
-        });
-    }
+	async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+		await provider.internal(via, {
+			value,
+			sendMode: SendMode.PAY_GAS_SEPARATELY,
+			body: beginCell().endCell(),
+		});
+	}
 
-    async getWriteDataChain(provider: ContractProvider, options: [Tuple, TupleItemInt]): Promise<any> {
-        const result = await provider.get('write_data_train', options);
-        return result.stack.readTuple();
-    }
+	async getWriteDataTrain(provider: ContractProvider, cells_train: Cell, boolStringData: string): Promise<any> {
+		const result = await provider.get('write_data_train', [
+			{type: 'slice', cell: cells_train},
+			{type: 'int', value: BigInt(parseInt(boolStringData, 2))}
+		]);
+		return result.stack.readCell();
+	}
 
-    async getChangedLinkedList(provider: ContractProvider, options: [TupleItemInt, TupleItemInt, TupleItemCell]): Promise<any> {
-        const result = await provider.get('find_and_replace', options);
-        return result.stack.readCell();
-    }
+	async getChangedLinkedList(provider: ContractProvider, boolStrFlag: string, boolValueStr: string, text: Cell): Promise<any> {
+		const result = await provider.get('find_and_replace', [
+			{type: 'int', value: BigInt(parseInt(boolStrFlag, 2))},
+			{type: 'int', value: BigInt(parseInt(boolValueStr, 2))},
+			{type: 'cell', cell: text},
+		]);
+		return result.stack.readCell();
+	}
 }
